1e19ad80ca8f38a0191d7afc90b6a44084f201b1
n-ruzhik <anastasiyaruzhetskaya@gmail.com>
2023-06-06 22:25:32 +0300

    add registration screen (with keyboard open)

--- Screens/RegistrationScreen.js
+++ Screens/RegistrationScreen.js
@@ -16,60 +16,54 @@ import { useFonts } from "expo-font";
 import { styles } from "../styles/RegistrationScreen.styles";
 
 export const RegistrationScreen = () => {
-  const [fontsLoaded] = useFonts({
-    RobotoRegular: require("../assets/fonts/Roboto-Regular.ttf"),
-    RobotoMedium: require("../assets/fonts/Roboto-Medium.ttf"),
-    RobotoRegular: require("../assets/fonts/Roboto-Bold.ttf"),
-  });
-  if (!fontsLoaded) {
-    return null;
-  }
-
   return (
-    // <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-    <View stele={styles.container}>
-      <ImageBackground
-        source={require("../assets/images/bg-photo.jpg")}
-        imageStyle={styles.image}
-      ></ImageBackground>
-      {/* <KeyboardAvoidingView
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <ImageBackground
+          source={require("../assets/images/bg-photo.jpg")}
+          imageStyle={styles.image}
+        ></ImageBackground>
+        <KeyboardAvoidingView
           behavior={Platform.OS === "ios" ? "padding" : "height"}
           style={styles.container}
-        > */}
-      <View style={styles.containerForm}>
-        <View style={styles.registrationFormBox}>
-          <View style={styles.photoBox}>
-            <Image
-              source={require("../assets/images/add.png")}
-              style={styles.addPhotoImg}
-            ></Image>
-          </View>
-          <Text style={styles.registerTitle}>Registration</Text>
-          <View style={styles.inputsContainer}>
-            <TextInput style={styles.input} placeholder="Login"></TextInput>
-            <TextInput style={styles.input} placeholder="Email"></TextInput>
-            <View style={styles.passwordContainer}>
-              <TextInput
-                style={styles.inputLast}
-                placeholder="Password"
-              ></TextInput>
-              <TouchableOpacity style={styles.showPasswordContainer}>
-                <Text styles={styles.showPasswordText}>Show</Text>
+        >
+          <View style={styles.containerForm}>
+            <View style={styles.registrationFormBox}>
+              <View style={styles.photoBox}>
+                <Image
+                  source={require("../assets/images/add.png")}
+                  style={styles.addPhotoImg}
+                ></Image>
+              </View>
+              <Text style={styles.registerTitle}>Registration</Text>
+              <View style={styles.inputsContainer}>
+                <TextInput style={styles.input} placeholder="Login"></TextInput>
+                <TextInput style={styles.input} placeholder="Email"></TextInput>
+                <View style={styles.passwordContainer}>
+                  <TextInput
+                    style={styles.inputLast}
+                    secureTextEntry={true}
+                    placeholder="Password"
+                  ></TextInput>
+                  <TouchableOpacity style={styles.showPasswordContainer}>
+                    <Text styles={styles.showPasswordText}>Show</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+              <TouchableOpacity title="Sign up" style={styles.signUpBtn}>
+                <Text style={styles.btnLabel}>Sign Up</Text>
               </TouchableOpacity>
+              <View>
+                <TouchableOpacity>
+                  <Text style={styles.textLogIn}>
+                    Already have account? Log In
+                  </Text>
+                </TouchableOpacity>
+              </View>
             </View>
           </View>
-          <TouchableOpacity title="Sign up" style={styles.signUpBtn}>
-            <Text style={styles.btnLabel}>Sign Up</Text>
-          </TouchableOpacity>
-          <View>
-            <TouchableOpacity>
-              <Text style={styles.textLogIn}>Already have account? Log In</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
+        </KeyboardAvoidingView>
       </View>
-      {/* </KeyboardAvoidingView> */}
-    </View>
-    // </TouchableWithoutFeedback>
+    </TouchableWithoutFeedback>
   );
 };
