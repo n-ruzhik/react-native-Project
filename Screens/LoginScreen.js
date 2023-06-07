91c465bf4efe2b2c98e6a97240a5081b533fd773
n-ruzhik <anastasiyaruzhetskaya@gmail.com>
2023-06-07 21:32:32 +0300

    add form logic ( registration + login screens credentials)

--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -9,15 +9,34 @@ import {
   Keyboard,
   Platform,
 } from "react-native";
+import React, { useState } from "react";
 import { useFonts } from "expo-font";
 import { styles } from "../styles/LoginScreen.styles";
 
+const initialState = {
+  email: "",
+  password: "",
+};
+
 export const LoginScreen = () => {
+  const [state, setState] = useState(initialState);
+  const [isShowPassword, setIsShowPassword] = useState(true);
+
   const [fontsLoaded] = useFonts({
     RobotoRegular: require("../assets/fonts/Roboto-Regular.ttf"),
     RobotoMedium: require("../assets/fonts/Roboto-Medium.ttf"),
     RobotoBold: require("../assets/fonts/Roboto-Bold.ttf"),
   });
+
+  const onLogin = () => {
+    console.log(state);
+    setState(initialState);
+  };
+
+  const handlePasswordVisibility = () => {
+    setIsShowPassword(!isShowPassword);
+  };
+
   if (!fontsLoaded) {
     return null;
   }
@@ -37,20 +56,34 @@ export const LoginScreen = () => {
             <View style={styles.loginFormThumb}>
               <Text style={styles.LoginTitle}>Log In</Text>
               <View style={styles.inputsContainer}>
-                <TextInput style={styles.input} placeholder="Email"></TextInput>
+                <TextInput
+                  style={styles.input}
+                  onChangeText={(text) => setState({ ...state, email: text })}
+                  value={state.email}
+                  placeholder="Email"
+                ></TextInput>
                 <View style={styles.passwordContainer}>
                   <TextInput
                     style={styles.inputLast}
+                    value={state.password}
+                    onChangeText={(text) =>
+                      setState({ ...state, password: text })
+                    }
                     placeholder="Password"
                     textContentType="password"
-                    secureTextEntry={true}
+                    secureTextEntry={!isShowPassword}
                   ></TextInput>
-                  <TouchableOpacity style={styles.showPasswordContainer}>
-                    <Text style={styles.showPasswordText}>Show</Text>
+                  <TouchableOpacity
+                    style={styles.showPasswordContainer}
+                    onPress={handlePasswordVisibility}
+                  >
+                    <Text style={styles.showPasswordText}>
+                      {!isShowPassword ? "Show" : "Hide"}
+                    </Text>
                   </TouchableOpacity>
                 </View>
               </View>
-              <TouchableOpacity title="Sign up" style={styles.logInBtn}>
+              <TouchableOpacity onPress={onLogin} style={styles.logInBtn}>
                 <Text style={styles.btnLabel}>Log In</Text>
               </TouchableOpacity>
               <View>
--- Screens/RegistrationScreen.js
+++ Screens/RegistrationScreen.js
@@ -12,15 +12,35 @@ import {
   Keyboard,
   Platform,
 } from "react-native";
+import React, { useState } from "react";
 import { useFonts } from "expo-font";
 import { styles } from "../styles/RegistrationScreen.styles";
 
+const initialState = {
+  userName: "",
+  email: "",
+  password: "",
+};
+
 export const RegistrationScreen = () => {
+  const [state, setState] = useState(initialState);
+  const [isShowPassword, setIsShowPassword] = useState(true);
+
   const [fontsLoaded] = useFonts({
     RobotoRegular: require("../assets/fonts/Roboto-Regular.ttf"),
     RobotoMedium: require("../assets/fonts/Roboto-Medium.ttf"),
     RobotoBold: require("../assets/fonts/Roboto-Bold.ttf"),
   });
+
+  const onRegister = () => {
+    console.log(state);
+    setState(initialState);
+  };
+
+  const handlePasswordVisibility = () => {
+    setIsShowPassword(!isShowPassword);
+  };
+
   if (!fontsLoaded) {
     return null;
   }
@@ -46,20 +66,41 @@ export const RegistrationScreen = () => {
               </View>
               <Text style={styles.registerTitle}>Registration</Text>
               <View style={styles.inputsContainer}>
-                <TextInput style={styles.input} placeholder="Login"></TextInput>
-                <TextInput style={styles.input} placeholder="Email"></TextInput>
+                <TextInput
+                  style={styles.input}
+                  value={state.userName}
+                  onChangeText={(text) =>
+                    setState({ ...state, userName: text })
+                  }
+                  placeholder="Login"
+                ></TextInput>
+                <TextInput
+                  style={styles.input}
+                  value={state.email}
+                  onChangeText={(text) => setState({ ...state, email: text })}
+                  placeholder="Email"
+                ></TextInput>
                 <View style={styles.passwordContainer}>
                   <TextInput
                     style={styles.inputLast}
-                    secureTextEntry={true}
+                    secureTextEntry={!isShowPassword}
+                    onChangeText={(text) =>
+                      setState({ ...state, password: text })
+                    }
+                    value={state.password}
                     placeholder="Password"
                   ></TextInput>
-                  <TouchableOpacity style={styles.showPasswordContainer}>
-                    <Text styles={styles.showPasswordText}>Show</Text>
+                  <TouchableOpacity
+                    style={styles.showPasswordContainer}
+                    onPress={handlePasswordVisibility}
+                  >
+                    <Text styles={styles.showPasswordText}>
+                      {!isShowPassword ? "Show" : "Hide"}
+                    </Text>
                   </TouchableOpacity>
                 </View>
               </View>
-              <TouchableOpacity title="Sign up" style={styles.signUpBtn}>
+              <TouchableOpacity onPress={onRegister} style={styles.signUpBtn}>
                 <Text style={styles.btnLabel}>Sign Up</Text>
               </TouchableOpacity>
               <View>
